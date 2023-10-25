ef761b00e33dae6c31fa341b9ebb54ec76f3a375
UltraFerous <jandoga31@gmail.com>
2023-10-25 14:35:06 -0400

    Organized the functions

--- play.js
+++ play.js
@@ -1,34 +1,7 @@
 const {connect} = require('./client');
+const {setupInput} = require('./input');
 
 connect();
-
-// setup interface to handle user input from stdin
-const handleUserInput = function(data) {
-  if (data.toLowerCase() === "w") {
-    console.log('move up');
-  }
-  if (data.toLowerCase() === "s") {
-    console.log('move down');
-  }
-  if (data.toLowerCase() === "a") {
-    console.log('move left');
-  }
-  if (data.toLowerCase() === "d") {
-    console.log('move right');
-  }
-
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+console.log("Connecting ...");
 
 setupInput();
